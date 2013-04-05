25e668fdc439e657ada0b6d8da072763e54c4b92
ArturT <arturtrzop@gmail.com>
2013-04-05 18:11:47 +0200

    Done about range

--- src/test/scala/org/functionalkoans/forscala/AboutRange.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutRange.scala
@@ -5,28 +5,30 @@ import support.KoanSuite
 class AboutRange extends KoanSuite {
 
   koan("Range are not inclusive at end of range") {
+    // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
     val someNumbers = Range(0, 10)
 
-    someNumbers.size should be(__)
+    someNumbers.size should be(10)
   }
 
   koan("Range can specify increment") {
+    // 2, 5, 8
     val someNumbers = Range(2, 10, 3)
 
-    someNumbers.size should be(__)
+    someNumbers.size should be(3)
   }
 
   koan("Range can indicate inclusion") {
     val someNumbers = Range(0, 34, 2)
-    someNumbers.contains(33) should be(__)
-    someNumbers.contains(32) should be(__)
-    someNumbers.contains(34) should be(__)
+    someNumbers.contains(33) should be(false) // not parity
+    someNumbers.contains(32) should be(true) // parity
+    someNumbers.contains(34) should be(false) // 34 is not in range
   }
 
   koan("Range can specify to include value") {
     val someNumbers = Range(0, 34).inclusive
 
-    someNumbers.contains(34) should be(__)
+    someNumbers.contains(34) should be(true)
   }
 
 }
