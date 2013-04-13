403aed34477813f9e0e983eae55a04577ea2f42b
ArturT <arturtrzop@gmail.com>
2013-04-13 18:40:28 +0200

    Small fix in about lists

--- src/test/scala/org/functionalkoans/forscala/AboutLists.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutLists.scala
@@ -106,7 +106,7 @@ class AboutLists extends KoanSuite {
   koan("Foldleft is like reduce, but with an explicit starting value") {
     val a = List(1, 3, 5, 7)
     // NOTE: foldLeft uses a form called currying that we will explore later
-    a.foldLeft(0)(_ + _) should equal(1 + 3 + 5 + 7)
+    a.foldLeft(0)(_ + _) should equal(0 + 1 + 3 + 5 + 7)
     a.foldLeft(10)(_ + _) should equal(10 + 1 + 3 + 5 + 7)
     a.foldLeft(1)(_ * _) should equal(1 * 1 * 3 * 5 * 7)
     a.foldLeft(0)(_ * _) should equal(0 * 1 * 3 * 5 * 7)
